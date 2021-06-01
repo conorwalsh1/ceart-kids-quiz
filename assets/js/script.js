18ee06996b0935c49a1b3f779be8d45303a5de36
conorwalsh1 <conorwalsh7@gmail.com>
2021-06-01 15:06:29 +0000

    Fix chrome tool error, used the wrong colon

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,20 @@
-const playButton = document.getElementByID('enter-quiz')
-const quizAreaElement = document.getElementById('quiz-area')
+const startButton = document.getElementById('start-btn')
+const questionContainerElement = document.getElementById('question-container')
 const questionElement = document.getElementById('question')
 const answerButtonsElement = document.getElementById('answer-buttons')
 
 let shuffledQuestions, currentQuestionIndex
 
-playButton.addEventListener('click', startQuiz)
+startButton.addEventListener('click', startQuiz)
 
 function startQuiz() {
     console.log('Started')
-    shuffledQuestions = questions.sort(() = Math.random() - .5)
-    currentQuestionIndex = 0
+    startButton.classList.add('hide')
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    questionContainerElement.classList.remove('hide')
     setNextQuestion()
+    currentQuestionIndex = 0
+    
 }
 
 function setNextQuestion () {
@@ -34,81 +37,81 @@ const questions = [
     {
         question: '1. If you have "dhá úll", how many apples do you have?',
         answers: [
-        { text: '1', correct = false },
-        { text: '2', correct = true },
-        { text: '3', correct = false },
+        { text: '1', correct: false },
+        { text: '2', correct: true },
+        { text: '3', correct: false }
         ]
     },
     {
         question: '2. If a house is painted the colour "dearg", what colour is it?',
         answers: [
-            { text: 'Black', correct = false },
-            { text: 'Red', correct = true },
-            { text: 'Blue', correct = false },
+            { text: 'Black', correct: false },
+            { text: 'Red', correct: true },
+            { text: 'Blue', correct: false }
             ]
     },
     {
         question: '3. "Bó" is the name of what farm animal?',
         answers: [
-            { text: 'Cow', correct = true },
-            { text: 'Pig', correct = false },
-            { text: 'Sheep', correct = false },
+            { text: 'Cow', correct: true },
+            { text: 'Pig', correct: false },
+            { text: 'Sheep', correct: false }
             ]
     },
     {
         question: '4. "Uisce" is the name for what kind of drink?',
         answers: [
-            { text: 'Milk', correct = false },
-            { text: 'Orange Juice', correct = false },
-            { text: 'Water', correct = true },
+            { text: 'Milk', correct: false },
+            { text: 'Orange Juice', correct: false },
+            { text: 'Water', correct: true }
             ]
     },
     {
         question: '5. If I go to the "siopa", where am I going?',
         answers: [
-            { text: 'Cinema', correct = false },
-            { text: 'Shop', correct = true },
-            { text: 'Library', correct = false },
+            { text: 'Cinema', correct: false },
+            { text: 'Shop', correct: true },
+            { text: 'Library', correct: false }
             ]
     },
     {
         question: '6. If Oisín said his hair is the colour "fionn", what colour is his hair?',
         answers: [
-            { text: 'Black', correct = false },
-            { text: 'Blonde', correct = true },
-            { text: 'Brown', correct = false },
+            { text: 'Black', correct: false },
+            { text: 'Blonde', correct: true },
+            { text: 'Brown', correct: false }
             ]
     },
     {
         question: '7. If the sky is the colour "gorm", what colour is the sky?',
         answers: [
-            { text: 'Blue', correct = true },
-            { text: 'Yellow', correct = false },
-            { text: 'Grey', correct = false },
+            { text: 'Blue', correct: true },
+            { text: 'Yellow', correct: false },
+            { text: 'Grey', correct: false }
             ]
     },
     {
         question: '8. "Corcaigh" is the name of which Irish county?',
         answers: [
-            { text: 'Dublin', correct = false },
-            { text: 'Cork', correct = true },
-            { text: 'Limerick', correct = false },
+            { text: 'Dublin', correct: false },
+            { text: 'Cork', correct: true },
+            { text: 'Limerick', correct: false }
             ]
     },
     {
         question: '9. Who is the "muinteoir" in the classroom?',
         answers: [
-            { text: 'Pupil', correct = false },
-            { text: 'Teacher', correct = true },
-            { text: 'Principal', correct = false },
+            { text: 'Pupil', correct: false },
+            { text: 'Teacher', correct: true },
+            { text: 'Principal', correct: false }
             ]
     },
     {
         question: '10. What is the Irish word for "bed"?',
         answers: [
-            { text: 'Leaba', correct = false },
-            { text: 'Leanach', correct = true },
-            { text: 'Leabhar', correct = false },
+            { text: 'Leaba', correct: false },
+            { text: 'Leanach', correct: true },
+            { text: 'Leabhar', correct: false }
             ]
     },
 ]
