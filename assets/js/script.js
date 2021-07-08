d745351e97cc914cf949eaf584cd6920b843836e
conorwalsh1 <conorwalsh7@gmail.com>
2021-07-08 08:13:01 +0000

    Remove do while loop as it is not needed for function to work

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,7 +10,6 @@ const buttonB = document.getElementById('btn--b');
 const buttonC = document.getElementById('btn--c');
 const buttonD = document.getElementById('btn--d');
 
-
 var score = 0;
 let i = 0;
 var questionCounter = 0;
@@ -47,9 +46,7 @@ function generateFirstQuestion(questionCounter) {
 
 function generateNextQuestion() {
     console.log('Next question loaded')
-    do {questionCounter++
-      }
-      while (questionCounter > 10);
+    questionCounter++
       if (questionCounter == 10) {
           console.log('Quiz End');
           document.getElementById('question').style.display = 'none';
@@ -68,13 +65,30 @@ function generateNextQuestion() {
 nextButton.addEventListener('click', generateNextQuestion);
 
 function correctAnswer() {
-    document.getElementById('btn--b').style.backgroundColor = "green"
-    document.getElementById('btn--a').style.backgroundColor = "red"
-    document.getElementById('btn--c').style.backgroundColor = "red"
-    document.getElementById('btn--d').style.backgroundColor = "red"
+    var theAnswer = questionSelection[questionCounter].correct
+    if (theAnswer == true) {
+        style.backgroundColor = "green"
+    } else {
+        style.backgroundColor = "red"
     }
+}
+buttonA.onclick = correctAnswer();
+buttonB.onclick = correctAnswer();
+buttonC.onclick = correctAnswer();
+buttonD.onclick = correctAnswer();
 
-    buttonB.addEventListener('click', incrementScore())
+
+
+
+
+
+    // document.getElementById('btn--b').style.backgroundColor = "green"
+    // document.getElementById('btn--a').style.backgroundColor = "red"
+    // document.getElementById('btn--c').style.backgroundColor = "red"
+    // document.getElementById('btn--d').style.backgroundColor = "red"
+    // }
+
+    // buttonB.addEventListener('click', incrementScore())
 
 function clearCorrectColor() {
     document.getElementById('btn--a').style.backgroundColor = "white"
@@ -82,11 +96,6 @@ function clearCorrectColor() {
     document.getElementById('btn--c').style.backgroundColor = "white"
     document.getElementById('btn--d').style.backgroundColor = "white"
 }
-
-nextButton.addEventListener('click', clearCorrectColor());
-
-
-    
 
 function incrementScore() {
     let oldScore = parseInt(document.getElementById("score-number").innerText);
@@ -105,41 +114,51 @@ var questionSelection = [
     {
         'question' : 'Question 1. If you have "dhá úll", how many apples do you have?',
         'answer' : ['1', '2', '3', '4'],
+        'correct' : '2'
     }, 
     {
         'question' : 'Question 2. If a house is painted the colour "dearg", what colour is it?',
-        'answer' : ['Black', 'Red', 'Blue', 'Pink']
+        'answer' : ['Black', 'Pink', 'Blue', 'Red'],
+        'correct' : 'Red'
     },
     {
         'question' : 'Question 3. "Bó" is the name of what farm animal?',
-        'answer' : ['Pig', 'Cow', 'Sheep', 'Goat']
+        'answer' : ['Pig', 'Sheep', 'Cow', 'Goat'],
+        'correct' : 'Cow'
     },
     {
         'question' : 'Question 4. "Uisce" is the name for what kind of drink?',
-        'answer' : ['Milk', 'Water', 'Orange Juice', 'Tea']
+        'answer' : ['Water', 'Milk', 'Orange Juice', 'Tea'],
+        'correct' : 'Water'
     },
     {
         'question' : 'Question 5. If I go to the "siopa", where am I going?',
-        'answer' : ['Cinema', 'Shop', 'Library', 'Beach']
+        'answer' : ['Cinema', 'Shop', 'Library', 'Beach'],
+        'correct' : 'Shop'
     },
     {
         'question' : "Question 6. John's hair is the colour 'dubh', what colour is John's hair?",
-        'answer' : ['Blonde', 'Black', 'Brown', 'Ginger']
+        'answer' : ['Blonde', 'Brown', 'Black', 'Ginger'],
+        'correct' : 'Black'
     },
     {
         'question' : 'Question 7. If the sky is the colour "gorm", what colour is the sky?',
-        'answer' : ['Yellow', 'Blue', 'Grey', 'Black']
+        'answer' : ['Yellow', 'Black', 'Grey', 'Blue'],
+        'correct' : 'Blue'
     },
     {
         'question' : 'Question 8. "Corcaigh" is the name of which Irish county?',
-        'answer' : ['Dublin', 'Cork', 'Limerick', 'Galway']
+        'answer' : ['Dublin', 'Cork', 'Limerick', 'Galway'],
+        'correct' : 'Cork'
     },
     {
         'question' : 'Question 9. Who is the "muinteoir" in the classroom?',
-        'answer' : ['Pupil', 'Teacher', 'Principal', 'Parent']
+        'answer' : ['Teacher', 'Pupil', 'Principal', 'Parent'],
+        'correct' : 'Teacher'
     },
     {
         'question' : 'Question 10. What is the Irish word for "bed"?',
-        'answer' : ['Leanach', 'Leaba', 'Leabhar', 'Lean']
+        'answer' : ['Leanach', 'Leabhar', 'Leaba', 'Lean'],
+        'correct' : 'Leaba'
     }
 ]
